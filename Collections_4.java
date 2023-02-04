e6c65c1407e08096f952335cb9e365e41ecb65c0
utpal4433 <122300998+utpal4433@users.noreply.github.com>
2023-02-04 20:53:40 +0530

    Create Collections_4.java

---
+++ Collections_4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Collections_4 {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.push("Ironman");
+        ll.push("Thor");
+        ll.push("Hulk");
+        ll.push("Loki");
+
+        System.out.println(ll.pop());
+    }
+}
